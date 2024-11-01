e71d0da0e9f18359f286d9cc79107109fce07fb5
Zaid Aly <zaidk4076@gmail.com>
2024-11-02 01:32:45 +0530

    api resource controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,12 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class CarController extends Controller
+class ProductController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
     public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
     {
         //
     }
@@ -26,6 +34,14 @@ class CarController extends Controller
      * Display the specified resource.
      */
     public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
     {
         //
     }
--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,15 @@
 <?php
 
+use App\Http\Controllers\CarController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
-// use App\Http\Controllers\CarController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::get('/car', [CarController::class, 'index']);
 
-
-//controller grouping 
-// Route::controller(CarController::class)->group(function(){
-//     Route::get('/car', 'index');
-//     Route::get('/my-car', 'my-car');
-// });
-
-
-// Route::get('/car', CarController::class);
+Route::apiResources([
+    'cars' => CarController::class,
+    'product' => ProductController::class,
+]);
